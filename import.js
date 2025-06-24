0be25d137bf4fb14ee43c0949a9030625934d1fd
Henry <37009172+henrycuongnguyen@users.noreply.github.com>
2025-06-24 09:51:21 +0700

    test get location db kiot

--- import.js
+++ import.js
@@ -71,8 +71,18 @@ async function importData() {
                     Type: type
                 });
                 await client.query(
-                    'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-                    [province.Id, province.Name, null, address, normalizedName, 6, "VN", 3, type]
+                    'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country", "IsActive","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+                    [province.Id, province.Name, null, address, normalizedName, 6, "VN", true, 3, type]
+                );
+            } else {
+                const dbProvince = dbprovinces.rows[0];
+                // If the province already exists, we can update IsActive = true
+                const address = province.Name;
+                const type = getLocationTypeValue(province.Name);
+                const normalizedName = normalizeName(province.Name);
+                await client.query(
+                    'UPDATE "Locations" SET "Name" = $1, "Address" = $2, "NormalizedName" = $3, "Type" = $4, "IsActive" = $5 WHERE "Id" = $6',
+                    [province.Name, address, normalizedName, type, true, dbProvince.Id]
                 );
             }
             var dbDistricts = await client.query(`SELECT * FROM "Locations" WHERE "ParentId" = $1`, [province.Id]);
@@ -83,6 +93,7 @@ async function importData() {
                 var dbWards = { rows: [] };
                 if (dbDistrict) {
                     id = dbDistrict.Id;
+                    await client.query('UPDATE "Locations" SET "IsActive" = $1 WHERE "Id" = $2', [true, id]);
                     dbWards = await client.query(`SELECT * FROM "Locations" WHERE "ParentId" = $1`, [id]);
                 } else {
                     id = GeneratorId(dbDistricts.rows, id);
@@ -102,8 +113,8 @@ async function importData() {
                         Type: type
                     });
                     await client.query(
-                        'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-                        [id, district.Name, province.Id, address, normalizedName, 6, "VN", 4, type]
+                        'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country","IsActive","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+                        [id, district.Name, province.Id, address, normalizedName, 6, "VN", true, 4, type]
                     );
                 }
 
@@ -118,6 +129,7 @@ async function importData() {
                     var type = getLocationTypeValue(ward.Name);
                     if (dbWard) {
                         wardId = dbWard.Id;
+                        await client.query('UPDATE "Locations" SET "IsActive" = $1 WHERE "Id" = $2', [true, wardId]);
                     } else {
                         wardId = GeneratorId(dbWards.rows, wardId);
                         imported.push({
@@ -126,14 +138,15 @@ async function importData() {
                             ParentId: id,
                             Address: address,
                             NormalizedName: normalizedName,
-                            Status: 5,
+                            Status: 6,
                             Country: "VN",
+                            IsActive: true,
                             Level: 5,
                             Type: type
                         });
                         await client.query(
-                            'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-                            [wardId, ward.Name, id, address, normalizedName, 5, "VN", 5, type]
+                            'INSERT INTO "Locations" ("Id", "Name", "ParentId","Address","NormalizedName","Status","Country","IsActive","Level","Type") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+                            [wardId, ward.Name, id, address, normalizedName, 6, "VN", true, 5, type]
                         );
                     }
                 }
@@ -154,7 +167,7 @@ async function importData() {
 
 function GeneratorId(data, id) {
     if (data.some(x => x.Id == id)) {
-        id += "_x"
+        id += "_X"
         return GeneratorId(data, id);
     }
     return id;
